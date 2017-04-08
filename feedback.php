6b1ba44e8074e1ad9f770f9d059a01b890dff012
Ivan Sitnikov <ivansit357@gmail.com>
2017-04-08 20:05:11 +1000

    first commit

---
+++ feedback.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Передача переменных</title>
+</head>
+<body>
+<?php
+
+?>
+	<form action="test.php" name="form_1" method="POST" target="_blank">
+		<p>Введите ваше имя: <br><input name="name" type="text" size="20"></p>
+		<p>Введите вашу фамилию: <br><input name="lastname" type="text" size="20"></p>
+		<p><input name="submit" type="submit" value="Отправить"></p>
+	</form>
+</body>
+</html>
---
+++ test.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Передача переменных методом post</title>
+</head>
+<body>
+<?php 
+	echo __FILE__;
+ ?>
+</body>
+</html>
